4f688f02f4147a83db2833d1222f868a6a5d8834
Gabriel <gabrielsoliveira043@gmail.com>
2021-07-22 07:29:53 -0300

    Finishing the application

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const buttonInsertText = document.querySelector(".btn-toggle");
 const divTextBox = document.querySelector(".text-box");
 const closeDivTextBox = document.querySelector(".close");
 const selectElement = document.querySelector("select");
+const buttonReadText = document.querySelector("#read");
+const textArea = document.querySelector("textarea")
 
 const humanExpressions = [
     { img: './img/drink.jpg', text: 'Estou com sede' },
@@ -19,34 +21,94 @@ const humanExpressions = [
     { img: './img/grandma.jpg', text: 'Quero ver a vovó' }
 ]
 
-const createExpressionBox = ({ img, text }) => {
-    const div = document.createElement("div");
+const utterance = new SpeechSynthesisUtterance();
 
-    div.classList.add("expression-box")
-    div.innerHTML = `
-        <img src="${img}" alt="${text}"/>
-        <p class="info">${text}</p>
-    `
+const setTextMessage = text => {
+    utterance.text = text;
+}
+
+const speakText = () => {
+    speechSynthesis.speak(utterance);
+}
+
+const setVoice = event => {
+    const selectedVoice = voices.find(voice => voice.name === event.target.value);
+    utterance.voice = selectedVoice;
+}
+
+const addExpressionBoxesIntoDOM = () => {
+    main.innerHTML = humanExpressions.map(({ img, text }) => `
+        <div class="expression-box" data-js="${text}">
+            <img src="${img}" alt="${text}" data-js="${text}"/>
+            <p class="info" data-js="${text}">${text}</p>
+        </div>
+
+    `).join("");
+}
+
+addExpressionBoxesIntoDOM();
+
+const setStyleOfClickedDiv = dataValue => {
+    const div = document.querySelector(`[data-js="${dataValue}"]`)
+
+    div.classList.add("active");
+    setTimeout(() => {
+        div.classList.remove("active");
+    }, 1000)
+}
+
+main.addEventListener("click", event => {
+    const clickedElement = event.target;
+    const clickedElementText = clickedElement.dataset.js;
+    const clickedElementMustBeSpoken = ["img", "p"].some(elementName => 
+        clickedElement.tagName.toLowerCase() === elementName.toLowerCase()
+    );
+
+    if(clickedElementMustBeSpoken) {
+        setTextMessage(clickedElement.dataset.js);
+        speakText();
+        setStyleOfClickedDiv(clickedElementText);
+    }
     
-    main.appendChild(div);
+})
+
+const insertOptionElementIntoDOM = voices => {
+    selectElement.innerHTML = voices.reduce((accumulator, {name, lang}) => {
+        accumulator += `<option value="${name}">${lang} | ${name}</option>`;
+        return accumulator;
+    }, "")
 
 }
 
-humanExpressions.forEach(createExpressionBox)
+const setUtteranceVoice = voice => {
+    
+    utterance.voice = voice;
+    const voiceOptionElement = selectElement
+        .querySelector(`[value="${voice.name}"]`);
+    
+    voiceOptionElement.selected = true;
+}
+
+const setPTBRVoices = voices => {
+    const googleVoice = voices.find(
+        voice => voice.name === "Google português do Brasil");
+    const microsoftVoice = voices.find(
+        voice => voice.name === "Microsoft Maria - Portuguese (Brazil)");
+
+    if (googleVoice) {
+        setUtteranceVoice(googleVoice);
+    } else if(microsoftVoice) {
+        setUtteranceVoice(microsoftVoice);
+    }
+}
 
 let voices = [];
 
 speechSynthesis.addEventListener("voiceschanged", () => {
     voices = speechSynthesis.getVoices();
 
-    voices.forEach(({name, lang}) => {
-        const option = document.createElement("option");
-        
-        option.value = name;
-        option.textContent = `${lang} | ${name}`
-        selectElement.appendChild(option);
-    })
-    
+    insertOptionElementIntoDOM(voices);
+    setPTBRVoices(voices);  
 })
 
 buttonInsertText.addEventListener("click", () => {
@@ -55,4 +117,11 @@ buttonInsertText.addEventListener("click", () => {
 
 closeDivTextBox.addEventListener("click", () => {
     divTextBox.classList.remove("show");
+})
+
+selectElement.addEventListener("change", setVoice);
+
+buttonReadText.addEventListener("click", () => {
+    setTextMessage(textArea.value);
+    speakText();
 })
